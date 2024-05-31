bbdbe1a931e596c49aa041b2c3425a9aac0751a5
hellen kimani <astridlee80@gmail.com>
2024-05-31 20:53:45 +0600

    my version

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import Header from './components/Header';
+import Base from './components/Base';
+import Toppings from './components/Toppings';
+import Order from './components/Order';
+import Home from './components/Home';
+
+const App = () => {
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
+
+  const addBase = (base) => {
+    setPizza({ ...pizza, base });
+  };
+
+  const addTopping = (topping) => {
+    let newToppings;
+    if (!pizza.toppings.includes(topping)) {
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter(item => item !== topping);
+    }
+    setPizza({ ...pizza, toppings: newToppings });
+  };
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/base" element={<Base addBase={addBase} pizza={pizza} />} />
+        <Route path="/toppings" element={<Toppings addTopping={addTopping} pizza={pizza} />} />
+        <Route path="/order" element={<Order pizza={pizza} />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
